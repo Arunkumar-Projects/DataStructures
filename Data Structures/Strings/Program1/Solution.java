6494f43081dbcc155fc21345131db29afee27a15
Arun <arunkumar.v@altiux.com>
2016-04-11 19:41:41 +0530

    committed the changes

---
+++ Data Structures/Strings/Program1/Solution.java
@@ -0,0 +1,37 @@
+package Program1;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        A.trim();
+		B.trim();
+        System.out.println(A.length()+B.length());
+        islexographicalOrder(A, B);
+       	String a=firstCharacterUpper(A);
+		String b=firstCharacterUpper(B);
+		System.out.println(a+" "+b);
+	}
+
+	private static String firstCharacterUpper(String a) {
+		
+		String output = a.substring(0, 1).toUpperCase() + a.substring(1);
+		return output;
+	}
+    
+    private static void islexographicalOrder(String a, String b) {
+		if (a.compareTo(b) > 0) {
+			System.out.println("Yes");
+		} else if (a.compareTo(b) < 0) {
+			System.out.println("No");
+		} else {
+			System.out.println("No");
+		}
+	}
+    
+}
