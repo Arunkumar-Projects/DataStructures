a155551a7de8ad260af6208c06b1f2a5a4dd2818
arun kumar <arunkumar.varada@gmail.com>
2016-09-13 18:59:42 +0530

    committed the two solution for splitting cicrcular lists into halfs

---
+++ Data Structures/Circular LinkedLists/com/java4u/ds/circularlinkedlists/SplitCircluarLinkedListIntoTwoHalfs.java
@@ -0,0 +1,77 @@
+package com.java4u.ds.circularlinkedlists;
+
+import java.util.LinkedList;
+
+import sun.applet.Main;
+
+public class SplitCircluarLinkedListIntoTwoHalfs {
+
+	static Node head, head1, head2;
+
+	static class Node {
+		private int data;
+		private Node next, prev;
+
+		Node(int data) {
+			this.data = data;
+			next = prev = null;
+		}
+	}
+
+	void printList(Node node) {
+		Node temp = node;
+        if (node != null) {
+            do {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            } while (temp != node);
+        }
+	}
+
+	void splitCircularList() {
+		Node slow_ptr = head;
+		Node fast_ptr = head;
+
+		if (head == null) {
+			return;
+		}
+
+		if (fast_ptr.next != head && slow_ptr.next.next != head) {
+			fast_ptr = fast_ptr.next.next;
+			slow_ptr = slow_ptr.next;
+		}
+
+		if (fast_ptr.next.next == head) {
+			fast_ptr = fast_ptr.next;
+		}
+
+		head1 = head;
+		if (head.next != head) {
+			head2 = slow_ptr.next;
+		}
+
+		fast_ptr.next = slow_ptr.next;
+		slow_ptr.next = head;
+	}
+	
+	public static void main(String[] args) {
+		SplitCircluarLinkedListIntoTwoHalfs list = new SplitCircluarLinkedListIntoTwoHalfs();
+		list.head = new Node(12);
+        list.head.next = new Node(56);
+        list.head.next.next = new Node(2);
+        list.head.next.next.next = new Node(11);
+        list.head.next.next.next.next = list.head;
+        
+        System.out.println("Original Circular Linked list ");
+        list.printList(head);
+ 
+        // Split the list
+        list.splitCircularList();
+        System.out.println("");
+        System.out.println("First Circular List ");
+        list.printList(head1);
+        System.out.println("");
+        System.out.println("Second Circular List ");
+        list.printList(head2);
+	}
+}
