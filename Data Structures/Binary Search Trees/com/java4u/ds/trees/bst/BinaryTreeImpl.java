a7badba998f08e9a2a657f15328ea420504cd008
arun kumar <arunkumar.varada@gmail.com>
2016-12-23 12:13:29 +0530

    added insert method impl

--- Data Structures/Binary Search Trees/com/java4u/ds/trees/bst/BinaryTreeImpl.java
+++ Data Structures/Binary Search Trees/com/java4u/ds/trees/bst/BinaryTreeImpl.java
@@ -3,37 +3,101 @@ package com.java4u.ds.trees.bst;
 import com.java4u.ds.trees.bst.BSTNode;
 
 public class BinaryTreeImpl {
-	
-	public BSTNode insert(int data) {
-		return null;
+	private int size = 0;
+	private BSTNode root = null;
+
+	public int getSize() {
+		return size;
+	}
+
+	public boolean isEmpty(BSTNode root) {
+		if (root == null) {
+			return true;
+		}
+		return false;
+	}
+
+	public void insert(BSTNode root, int data) {
+		BSTNode node = new BSTNode(data);
+		if (isEmpty(root)) {
+			root = node;
+			size++;
+			return;
+		}
+		while (root != null) {
+			if (data < root.getData()) {
+				if (root.getLeft() == null) {
+					root.setLeft(node);
+					size++;
+					return;
+				} else {
+					root.setLeft(root);
+				}
+			} else if (data >= root.getData()) {
+				if(root.getRight()== null){
+					root.setRight(node);
+					size++;
+					return;
+				}else{
+					root.setRight(root);
+				}
+			}
+		}
+
 	}
 
 	public void delete(int data) {
 
 	}
 
-	public int findMax(BSTNode root) {
+	public int findMaxIterative(BSTNode root) {
 		if (root == null) {
-			return 0;
-		} else {
+			System.out.println("Invalid Data!!");
+			return -1;
+		}
+		while (root.getRight() != null) {
+			root.setRight(root.getRight());
 			if (root.getRight() == null) {
 				return root.getData();
 			}
-			findMax(root.getRight());
 		}
-		return 0;
+		return -1;
 	}
 
-	public int findMin(BSTNode root) {
+	public int findMinIterative(BSTNode root) {
 		if (root == null) {
-			return 0;
-		} else {
+			System.out.println("Invalid Data!!");
+			return -1;
+		}
+		while (root.getLeft() != null) {
+			root.setLeft(root.getLeft());
 			if (root.getLeft() == null) {
 				return root.getData();
 			}
-			findMin(root.getLeft());
 		}
-		return 0;
+		return -1;
+	}
+
+	public int findMaxRecursive(BSTNode root) {
+		if (root == null) {
+			return -1;
+		} else if (root.getRight() == null) {
+			return root.getData();
+		} else {
+			findMaxRecursive(root.getRight());
+		}
+		return -1;
+	}
+
+	public int findMinRecursive(BSTNode root) {
+		if (root == null) {
+			return -1;
+		} else if (root.getLeft() == null) {
+			return root.getData();
+		} else {
+			findMinRecursive(root.getLeft());
+		}
+		return -1;
 	}
 
 	public void inOrderTraversal(BSTNode root) {
