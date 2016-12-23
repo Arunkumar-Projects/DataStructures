3e80d4214268018b8c239d05f6b5b70513a99058
arun kumar <arunkumar.varada@gmail.com>
2016-12-23 11:40:39 +0530

    committed BST tree impl

--- Data Structures/Binary Search Trees/com/java4u/ds/trees/bst/BinaryTreeImpl.java
+++ Data Structures/Binary Search Trees/com/java4u/ds/trees/bst/BinaryTreeImpl.java
@@ -1,6 +1,9 @@
 package com.java4u.ds.trees.bst;
 
+import com.java4u.ds.trees.bst.BSTNode;
+
 public class BinaryTreeImpl {
+	
 	public BSTNode insert(int data) {
 		return null;
 	}
@@ -9,27 +12,61 @@ public class BinaryTreeImpl {
 
 	}
 
-	public int findMax() {
+	public int findMax(BSTNode root) {
+		if (root == null) {
+			return 0;
+		} else {
+			if (root.getRight() == null) {
+				return root.getData();
+			}
+			findMax(root.getRight());
+		}
 		return 0;
 	}
 
-	public int findMin() {
+	public int findMin(BSTNode root) {
+		if (root == null) {
+			return 0;
+		} else {
+			if (root.getLeft() == null) {
+				return root.getData();
+			}
+			findMin(root.getLeft());
+		}
 		return 0;
 	}
 
-	public void inOrderTraversasl() {
+	public void inOrderTraversal(BSTNode root) {
+		if (root == null) {
+			return;
+		} else {
+			inOrderTraversal(root.getLeft());
+			System.out.println(root.getData());
+			inOrderTraversal(root.getRight());
+		}
+	}
+
+	public void levelOrderTraversal(BSTNode root) {
 
 	}
 
-	public void levelOrderTraversal() {
-
+	public void preOrderTraversal(BSTNode root) {
+		if (root == null) {
+			return;
+		} else {
+			System.out.println(root.getData());
+			preOrderTraversal(root.getLeft());
+			preOrderTraversal(root.getRight());
+		}
 	}
 
-	public void preOrderTraversal() {
-
-	}
-
-	public void postOrderTrvaersal() {
-
+	public void postOrderTrvaersal(BSTNode root) {
+		if (root == null) {
+			return;
+		} else {
+			postOrderTrvaersal(root.getLeft());
+			postOrderTrvaersal(root.getRight());
+			System.out.println(root.getData());
+		}
 	}
 }
