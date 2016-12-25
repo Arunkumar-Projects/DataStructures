71162137bb73b490d771d0d9beb558a3d0cff39c
arun kumar <arunkumar.varada@gmail.com>
2016-12-25 16:13:04 +0530

    committed base class for the minmax in array

---
+++ Data Structures/Arrays/com/java4u/ds/arrays/MinAndMaxINArray.java
@@ -0,0 +1,19 @@
+package com.java4u.ds.arrays;
+
+public class MinAndMaxINArray {
+	private int min = Integer.MAX_VALUE;
+	private int max = Integer.MIN_VALUE;
+
+	public int getMin(int a[], int low, int high) {
+		return min;
+	}
+
+	public int getMax(int a[], int low, int high) {
+		return max;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
