892f5c124d52c82d3970cdeadaeddd86518383a4
arun kumar <arunkumar.varada@gmail.com>
2016-12-28 00:00:12 +0530

    committed code

--- Data Structures/Binary Trees/com/java4u/ds/trees/binarytree/CreateMirrorTree.java
+++ Data Structures/Binary Trees/com/java4u/ds/trees/binarytree/CreateMirrorTree.java
@@ -2,4 +2,14 @@ package com.java4u.ds.trees.binarytree;
 
 public class CreateMirrorTree {
 
+	public BTNode createMirrorTree(BTNode root) {
+		if (root == null) {
+			return null;
+		} else {
+			BTNode node = new BTNode(root.getData());
+			node.setLeft(createMirrorTree(root.getRight()));
+			node.setRight(createMirrorTree(root.getLeft()));
+			return node;
+		}
+	}
 }
