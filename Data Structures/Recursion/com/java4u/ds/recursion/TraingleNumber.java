6052601e430a1c495dc96fa9d5c19c0edd561859
arun kumar <arunkumar.varada@gmail.com>
2017-01-04 18:22:17 +0530

    committed

--- Data Structures/Recursion/com/java4u/ds/recursion/TraingleNumber.java
+++ Data Structures/Recursion/com/java4u/ds/recursion/TraingleNumber.java
@@ -1,9 +1,18 @@
-package com.java4u.ds.recursion.triaingle;
+package com.java4u.ds.recursion;
 
 public class TraingleNumber {
 
+	public static int traingleNumber(int n) {
+		if (n <= 1) {
+			return 1;
+		} else {
+			return n + traingleNumber(n - 1);
+		}
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number = 10;
+		System.out.println("Traingle Number of " + number + " is :: " + traingleNumber(number));
 
 	}
 
