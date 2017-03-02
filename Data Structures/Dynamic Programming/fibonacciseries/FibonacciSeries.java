0cfcbeef857874d890351d04a0f54665a5e6da6c
arun kumar <arunkumar.varada@gmail.com>
2017-03-02 21:22:58 +0530

    committed fibonacci with memoization

--- Data Structures/Dynamic Programming/fibonacciseries/FibonacciSeries.java
+++ Data Structures/Dynamic Programming/fibonacciseries/FibonacciSeries.java
@@ -1,7 +1,7 @@
 package fibonacciseries;
 
 public class FibonacciSeries {
-
+	private static long[] fibArray = new long[100];
 	int fibonacci(int n) {
 		if (n == 0) {
 			return 0;
@@ -13,10 +13,26 @@ public class FibonacciSeries {
 		}
 	}
 
+	public static long fibonacciWithMemoization(int n) {
+		long fibValue = 0;
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else if (fibArray[(int) n] != 0) {
+			return fibArray[(int) n];
+		} else {
+			fibValue = fibonacciWithMemoization(n - 1) + fibonacciWithMemoization(n - 2);
+			fibArray[(int) n] = fibValue;
+			return fibValue;
+		}
+	}
+	
 	public static void main(String[] args) {
 		int n=82;
 		FibonacciSeries solution= new FibonacciSeries();
-		System.out.println(solution.fibonacci(n));
+		//System.out.println(solution.fibonacci(n));
+		System.out.println(fibonacciWithMemoization(n));
 	}
 
 }
